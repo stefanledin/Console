91f4d3901772345d706f181033d46431293cf596
stefanledin <stefan.ledin@gmail.com>
2012-12-06 06:19:37 +0100

    Working on a autocomplete-dropdown

--- test.js
+++ test.js
@@ -15,21 +15,44 @@
 
     Stefan.work = 'Web developer';
 
+    Stefan.interests = '["Web development", "Sports", "Apple"]';
+
+    Stefan.skills = '["HTML", "CSS", "Javascript", "jQuery", "PHP", "MySQL", "CodeIgniter", "Laravel", "Wordpress", "Drupal", "Facebook API", "Twitter API", "LinkedIn API", "Google Maps API", "Responsive Web Design", "Regular Expressions", "jQuery Mobile", "Photoshop", "Illustrator", "InDesign", "Flash", "Git"]';
+
+    Stefan.learning = '["Backbone.js", "Ruby", "Ruby On Rails", "Sinatra"]';
+
     var commands = [
         'stefan.presentation()',
         'stefan.age',
         'stefan.location',
         'stefan.location.city',
         'stefan.location.country',
-        'stefan.work'
+        'stefan.work',
+        'stefan.interests',
+        'stefan.skills',
+        'stefan.learning'
     ];
+
+    for (var i = commands.length - 1; i >= 0; i--) {
+        $('datalist#prompt').append('<option value="'+commands[i]+'">')
+        console.log(commands[i]);
+    };
     
     $('input').on('keyup', function(e) {
-        
+
+        var value = $(this).val().toLowerCase();
+        var regexp = new RegExp('^'+value, 'g');
+
+        if ( value.length != 0 && 'stefan'.search(regexp) != '-1' ) {
+            console.log('visa');
+            $('datalist#prompt').show();
+        } else {
+            console.log('dölj');
+            $('datalist#prompt').hide();
+        }
+
         if (e.keyCode == 13) {
             
-            var value = $(this).val().toLowerCase();
-            console.log(value);
             var arrCheck = $.inArray( value, commands );
             
             switch (arrCheck) {
@@ -56,6 +79,18 @@
                 // Stefan.work
                 case 5:
                     $('#console').append('<li><span class="command">Stefan.work</span><br>'+Stefan.work+'</li>');
+                break;
+                // Stefan.interests
+                case 6:
+                    $('#console').append('<li><span class="command">Stefan.interests</span><br>'+Stefan.interests+'</li>');
+                break;
+                // Stefan.skills
+                case 7:
+                    $('#console').append('<li><span class="command">Stefan.skills</span><br><span class="array">'+Stefan.skills+'</span></li>');
+                break;
+                // Stefan.learning
+                case 8:
+                    $('#console').append('<li><span class="command">Stefan.learning</span><br><span class="array">'+Stefan.learning+'</span></li>');
                 break;
 
                 // Error
